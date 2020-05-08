c0d3d1551a191bb03b01b483a500b4fdd820982b
Agus R Siahaan <agusronaldo2@gmail.com>
2020-05-07 20:55:10 -0700

    sqlite database

--- app/Http/Controllers/CustomersController.php
+++ app/Http/Controllers/CustomersController.php
@@ -3,17 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Customer;
 
 class CustomersController extends Controller
 {
     public function list()
     {
-    	$customers = [
-		'Agus',
-		'Ronaldo',
-		'Siahaan',
-		'Agus Ronaldo Siahaan',
-		];
+    	$customers = Customer::all();
+  		//$customers = [
+		// 'Agus',
+		// 'Ronaldo',
+		// 'Siahaan',
+		// 'Agus Ronaldo Siahaan',
+		// ];
 
 		return view('customers', [
 			'customers' => $customers,
--- resources/views/customers.blade.php
+++ resources/views/customers.blade.php
@@ -12,17 +12,17 @@
 </ul> -->
 
 
-<ul>
+<!-- <ul>
 	<li><a href="/">Home</a></li>
 	<li><a href="/about">About Us</a></li>
 	<li><a href="/contact">Contact Us</a></li>
 	<li><a href="/customers">Customers</a></li>
 
-</ul>
+</ul> -->
 
 <ul>
 	@foreach($customers as $customer)
-		<li>{{$customer}}</li>
+		<li>{{$customer->name}}</li>
 	@endforeach
 </ul>
 
